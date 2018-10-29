885d5cbc514fc612c7292d2ebc45d42d543b0f04
Will Rolls <32841471+TreeFrogBandit@users.noreply.github.com>
2018-10-29 15:29:44 +0000

    dump all of the working out into the same place

---
+++ convert hex to rgb.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mplc
+
+
+x = ["#011c41","#F2E8C3","#F5A219","#F27612","#DA2A04"]
+for i in x:
+    print(mplc.hex2color(i))
---
+++ html.py
@@ -0,0 +1,30 @@
+head = """<!DOCTYPE html>
+<html>
+    <head>
+        <title>Output</title>
+        <style>
+            html, body {
+                margin: 10px;
+                padding: 50px;
+                }
+
+            p {
+                text-align: center;
+                padding: 50px;
+                line-height: 1.15;
+                font-size: 30px;
+                font-variant: small-caps;
+                font-family: Bookman, Arial, Helvetica, sans-serif;
+                font-weight: bold;
+                }
+
+            id='container'{
+                padding-20px
+                }
+
+        </style>
+    </head>
+    <body>"""
+
+foot="""</body>
+</html>"""
